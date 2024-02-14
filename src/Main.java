e82f4635a9d5f973cc82f6abd56dd58562aebcd5
nurkyzzikirdinova <nzikirdinova7@gmail.com>
2024-02-14 19:19:53 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Консольго 2 ден 9 га чейинки таблица умноженияны чыгарыныз. Программаны бир иштеткенде бары чыксын
+//        //
+//        for (int i = 2; i <=9 ; i++) {
+//            for (int j = 1; j <= 10; j++) {
+//                System.out.println(i+"*"+j+"="+i*j);
+
+        for (int i = 1; i <=31 ; i++) {
+            for (int j = 3; j < 5; j++) {
+                System.out.println(i + j);
+            }
+        }
+            }
+        }
